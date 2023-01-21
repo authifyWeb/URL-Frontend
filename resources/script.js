a6d8eaa47ec0a707fbf2e5f7bf007cd48926527b
webVerts <100699190+webVerts@users.noreply.github.com>
2023-01-21 16:25:20 +0530

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -3,29 +3,32 @@ async function fetch_Data()
 			const res=await fetch ("https://raw.githubusercontent.com/authifyWeb/authifyURL-Database/main/everything.json");
 			const response=await res.json();
 			var json = response.source;
-			
+			//console.log(response.source)
 			
 }
 fetch_Data();
 function myFunction(){ if (event.key === "Enter"){searchNow();}}
+
 function searchNow()
 {	var input = document.getElementById('link_id').value;
 		
 	document.getElementById("data").innerHTML ="";
 	document.getElementById("disclaimer").innerHTML ="";
 	var url = new URL(input);
-	
+	console.log(url)
 	var hostname = url.hostname;
 	var protocol = url.protocol;
 	var origin = url.origin;
 	var href= url.href;
 	var pathname= url.pathname;
 
-	
+	//console.log(hostname, protocol, origin, href)
+	//console.log(hostname+pathname)
 
 	var output= authification(url, href, origin, hostname,protocol,pathname); 
 		
-			data.innerHTML = (output );
+			data.innerHTML = (output || "...Verifying");
+			
 }
 function authification(url, href, origin, hostname,protocol,pathname)
 {
@@ -41,17 +44,17 @@ function authification(url, href, origin, hostname,protocol,pathname)
 				return output;
 				
 			}
-	else if(origin =="https://twitter.com") {link = hostname + pathname.toLowerCase(); var output = compare(link);
+		else if(origin =="https://twitter.com") {link = hostname + pathname.toLowerCase(); var output = compare(link);
 				data.innerHTML= output;
 				return output ;}
-	else{var output= compare(hostname);
+		else{var output= compare(hostname);
 				return output ;
 			}
 }
 
 function compare(link)
-{	fetchData(link);
-	async function fetchData(link) 
+{	fetchData();
+	async function fetchData() 
 	{
 		const res=await fetch ("https://raw.githubusercontent.com/authifyWeb/authifyURL-Database/main/everything.json");
 			const response=await res.json();
@@ -63,7 +66,7 @@ function compare(link)
 					
 					if(json[i].urls[j] == link )
 					{ 
-						var Data=`<h5 style="color:white";> ` + link + ` <p> <span style="color:#A2FB15; font-size: 16px;">This site is verified by authifyURL.</span> &nbsp;<span class="tooltip"> ✅ <span class="tooltiptext">You can use this website with complete trust. </span> </p> <p><span>Organisation : </span>` +response.source[i].name+ `</p></h5>` ;
+						var Data=`<h5 style="color:white";>  ` + link + ` <p></br> <span style="color:#A2FB15; font-size: 16px; ">This site is verified by authifyURL.</span> &nbsp;<span class="tooltip"> ✅ <span class="tooltiptext">This website is valid and trustworthy. </span> </p> <p><span>Organisation : </span>` +response.source[i].name+ `</p></h5>` ;
 						
 						var Disclaimer=`Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white; target ="_blank"> how we verify</a> what is valid and what is not.`;
 						data.innerHTML= Data;
@@ -71,7 +74,7 @@ function compare(link)
 					return;
 					}
 					else{
-						var Data = `<h5 style=" color:white"; >` + link + `<p><span style="color:red;font-size: 16px;"> We weren't able to verify this site. Please be cautioned continuing in this site.</span> &nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">This doesn't mean that it's a fake website, it's just not in our database. </span> </p> </h5>` ;
+						var Data = `<h5 style=" color:white"; >` + link + `<p><span style="color:red;font-size: 16px;"> We weren't able to verify this site. Please be cautioned continuing in this site.</span> &nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">Possibly scam. If unsure please take help from someone you know. </span> </p> </h5>` ;
 						var Disclaimer=`Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white; target ="_blank"> how we verify</a> what is valid and what is not.`;
 						data.innerHTML= Data;
 						disclaimer.innerHTML=Disclaimer;
