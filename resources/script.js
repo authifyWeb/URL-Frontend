e23c1da9be2d1c755a53e78ab15b1ab248fd922d
webVerts <webverts@protonmail.com>
2023-04-05 16:44:28 +0530

    Redesign Homepage
    Website was redesigned so that it can be scaled, without compromising on time.
    Data is sourced using REST API, so users need not download the whole json file which possibly may slow down the entire site, once more data is added. Also a pro when users have slow internet.
    The new design will be similar to that of authifyFame.
    
    Co-Authored-By: authifyAlly <117624289+authifyAlly@users.noreply.github.com>

--- resources/script.js
+++ resources/script.js
@@ -1,13 +1,111 @@
-async function fetch_Data() 
-{
-			const res=await fetch ("https://raw.githubusercontent.com/authifyWeb/authifyURL-Database/main/everything.json");
-			const response=await res.json();
-			var json = response.source;
-			//console.log(response.source)
-			
+const el = document.querySelector(".searchterm");
+var authData;
+
+
+/* Initialising typeahead-standalone.js: https://typeahead.digitalfortress.tech/ */
+
+typeahead({
+    input: el,
+    source: {
+    remote: {
+        url: "http://apiurl-theta.vercel.app/api/products?alias=%QUERY", 
+		wildcard: "%QUERY",
+
+
+      requestOptions: {}
+        },
+        
+		
+		
+	identifier: 'alias',
+	
+    transform: (data) => { 
+    return data.Source;
+    },
+	
+  identity: (suggestion) => `${suggestion.name}`
+	},
+	
+	
+  display: (item, event) => {
+    
+
+    return `${item.name}`;
+	 },
+	
+
+
+debounceRemote: 300,
+highlight: true,
+className: 'typeahead-sgst',
+minLength: 3,
+preventSubmit: false,
+limit:25,
+hint: false,
+autoSelect: false,
+templates: {
+    suggestion: (authifyURL) => (
+		
+    `<div  class="single-item">
+      <img src=${authifyURL.logo} style="background-color:${authifyURL.bg}; font-size:12px;" loading="lazy">
+      <div class="details">
+      <div class="company_name"><span>${authifyURL.name}</span><span> <img class="badge" src="images/goldbadge.svg"></span></div>
+	  <div class="company_type">${authifyURL.cat}: ${authifyURL.origin} </div>
+    </div></div>`
+    ),
+   
+header: (resultSet) => `Results for <span style="color:#A2FB15; background-color:#1f282d; text-transform: uppercase;">" ${resultSet.query} "</span> : ${resultSet.count} `,
+notFound: (resultSet) => {   return `Nothing found for <span style="color:yellow; background-color:#1f282d; text-transform: uppercase;">" ${resultSet.query} "</span>`;},
+
+footer:() => 'an <a href="https://authifyweb.com" target="_blank" style="color:goldenrod; font-size:14px;"> authifyWeb</a> project',
+loader: () => '<div style="text-align: center"><img src="images/loader.svg" /></div>',
+},
+
+onSubmit: (e, selectedSuggestion) => {
+
+
+
+ 
+if (selectedSuggestion) {
+
+ document.getElementById('output'),innerHTML ="";
+ document.getElementById("compatible").innerHTML ="";
+const myJSON = JSON.stringify(selectedSuggestion);
+const outp=JSON.parse(myJSON);
+const {name, urls,type,id,official} = outp;
+console.log(myJSON)
+//console.log(outp)
+authData=outp;
+
+
+
+
+
+
+
+
+
+document.getElementById('naam').innerHTML = `<p style="font-size:1em;"> Submit URL below to verify whether the site belongs to <span style="color:#DFB014 "> ${name} </span></> </p>`;
+
+var form=`<input type='url' id='link_id'  autocomplete="off" placeholder="Paste the URL" > `;
+var btn=`<input type="submit" id="search_btn" value="Search 🔍" onClick="searchNow()" > <p style="color:white; font-size:12px; padding-top:2px;">Please use a valid URL format (with https://). Or else no data will be displayed.</p><div class="refresh_btn_box" style="margin-top:5px; margin-bottom:5px;"><button type="button"; onClick="window.location.reload()">Search Another Link </button></div> `;
+input_form.innerHTML=form;
+input_btn.innerHTML=btn;
+
+
+
+
 }
-fetch_Data();
-link_id.addEventListener("keydown", (event) => {
+
+}
+
+
+
+});	 
+
+
+
+input_form.addEventListener("keydown", (event) => {
     if (event.defaultPrevented) {
       return; // Do nothing if the event was already processed
     }
@@ -29,102 +127,71 @@ link_id.addEventListener("keydown", (event) => {
     // Cancel the default action to avoid it being handled twice
     event.preventDefault();
   }, true);
-
+  
 function searchNow()
-{	var input = document.getElementById('link_id').value;
-		
+{   var input = document.getElementById('link_id').value;
+	
+	
 	document.getElementById("data").innerHTML ="";
 	document.getElementById("disclaimer").innerHTML ="";
-	var url = new URL(input);
-	//console.log(url)
-	var hostname = url.hostname;
-	var protocol = url.protocol;
-	var origin = url.origin;
-	var href= url.href;
-	var pathname= url.pathname;
+    var url = new URL(input);
+	
+    var hostname = url.hostname;
+    var protocol = url.protocol;
+    var origin = url.origin;
+    var href= url.href;
+    var pathname= url.pathname;
 
-	//console.log(hostname, protocol, origin, href)
-	//console.log(hostname+pathname)
-
-	var output= authification(url, href, origin, hostname,protocol,pathname); 
+    var output= authification(url, href, origin, hostname,protocol,pathname); 
 		
 			data.innerHTML = (output || "...Verifying");
 			
+
+
+
+
 }
+
 function authification(url, href, origin, hostname,protocol,pathname)
 {
-	if (protocol == "about:" || protocol == "chrome:" || protocol == "edge:" || protocol =="view-source:" )
-	{
-		return `<h3> This page is part of your browser<h3>`;
-	}
-	else if(protocol != "https:" ) {return `<h3> This website is not secure. Please refrain from submitting personal data and don't download files from such sources</h3>`;}
-	
-	else if(origin=="https://duckduckgo.com")
-		{ if(pathname=="/") return `<p> This is DuckDuckGo Search Results page. Be wary of the links you click from a results page.</p>`;
-		  else{ link=hostname; var output = compare(link); return output;}
-		}
-	else if(hostname=="www.google.com"||hostname=="www.google.ca"||hostname=="www.google.co.in"||hostname=="www.google.co.uk"||hostname=="europe.google.com"){
-		var hostname="www.google.com"; 				
-			if(pathname=="/search") /*||pathname=="/"||pathname=="/webhp"*/
-			{
-				return `<p> This is Google Search Results page. Be wary of the links you click from a results page.</p>`;
-			}
-			else{link=hostname; var output=compare(link); return output;}
-
-	}
-
-	else if(origin=="https://www.bing.com") 
-		{ if(pathname=="/search" || pathname=="/shop") { return `<p> This is Microsoft Bing Search Results page. Be wary of the links you click from a results page.</p>`;}
-		else{ link=hostname; var output = compare(link); return output;}
-		}
-	else if(origin=="https://search.brave.com") 
-		{ return `<p> This is Brave Search Results page. Be wary of the links you click from a results page.</p>`;}
-	else if(hostname=="search.yahoo.com"||hostname=="in.search.yahoo.com"||hostname=="uk.search.yahoo.com"||hostname=="us.search.hostname.com"){
-		
-		if(pathname.split(';')[0]=="/search")
-		{
-			return `<p>This is Yahoo Search Results page. Be wary of the links you click from a results page.</p>`;
-		}
-	}			
-	
+	if(protocol != "https:" ) {return `<p style="padding:40px; color:lightred;"> This website is not secure. Please refrain from submitting personal data and don't download files from such sources</p>`;}
 	else if( origin =="https://www.facebook.com" )  
 			{	
 				link= hostname+'/'+pathname.split('/')[1];
 				var output = compare(link);
 				return output;
 			}
-		else if(origin =="https://twitter.com")
+	else if(origin =="https://twitter.com")
 			{
 				
 				link=hostname+'/'+pathname.split('/')[1].toLowerCase();
 				var output = compare(link);	
 				return output;
 			}
-			/* Youtube has 3 different types of URLS. github.com/authifyWeb/authifyNow/issues/14#issuecomment-1403993482 */
-		else if(origin=="https://www.youtube.com")
-				{
-					var channel=(pathname.split('/')[1]);
-					if(channel=="channel") { link = hostname +'/' +pathname.split('/')[1]+ '/' + pathname.split('/')[2];}
-					else if(channel=="c") { 
-					var id=pathname.split('/')[2].toLowerCase();
-					link= hostname+'/'+id}
-					else{link=hostname+'/'+pathname.split('/')[1].toLowerCase();}
-					var output = compare(link);	
-					return output;
-				}
-		else if( origin=="https://www.twitch.tv" || origin=="https://www.instagram.com" )
+	else if(origin=="https://www.youtube.com")
+      {
+        var channel=(pathname.split('/')[1]);
+        if(channel=="channel") { link = hostname +'/' +pathname.split('/')[1]+ '/' + pathname.split('/')[2];}
+        else if(channel=="c") { 
+        var id=pathname.split('/')[2].toLowerCase();
+        link= hostname+'/'+id}
+        else{link=hostname+'/'+pathname.split('/')[1].toLowerCase();}
+        var output = compare(link);	
+        return output;
+      }
+	else if( origin=="https://www.twitch.tv" || origin=="https://www.instagram.com" )
 			{
-				link = hostname +'/' + pathname.split('/')[1].toLowerCase(); 
+				link = hostname + pathname.toLowerCase(); 
 				var output = compare(link); 
-			
+			  return output;
 			}
-		else if(origin=="https://www.reddit.com" || origin=="https://old.reddit.com")
+	else if(origin=="https://www.reddit.com" || origin=="https://old.reddit.com")
 			{	
 				link=hostname +'/' +pathname.split('/')[1]+ '/' + pathname.split('/')[2].toLowerCase();
 				var output = compare(link);
-				
-				}
-		else if(origin == "https://github.com")
+				return output;
+			}
+	else if(origin == "https://github.com")
 				{
 				var id= pathname.split('/')[1];
 					
@@ -137,75 +204,67 @@ function authification(url, href, origin, hostname,protocol,pathname)
 				var output = compare(link);	
 				return output;	
 				}
-		else if(origin == "https://ko-fi.com" || origin =="https://www.buymeacoffee.com" || origin=="https://liberapay.com" || origin =="https://opencollective.com")
+	else if(origin == "https://ko-fi.com" || origin =="https://www.buymeacoffee.com" || origin=="https://liberapay.com" || origin =="https://opencollective.com")
 				{
 					
 					link=hostname+'/'+pathname.split('/')[1].toLowerCase();
 					var output = compare(link);
 					return output ;
 				}
-		else if(origin=="https://www.patreon.com")
-				{	
-					var id=pathname.split('/')[1];
-						if(id=="join")
-							{ var link= hostname+'/'+pathname.split('/')[2].toLowerCase();}
-						else { var link = hostname+'/'+pathname.split('/')[1].toLowerCase(); }
-					var output = compare(link);	
-					return output;	
-				
-				}
-			// Filter for crowdfunding platforms
-		else if(origin=="https://milaap.org"|| origin=="https://www.impactguru.com"|| origin=="https://www.ketto.org")
-				{var output = `Though the website is valid, authifyURL can't be used to verify the legalility of these fundraisers, please contact them directly.`;
-				return output;	}
+		
 					
-		else
-			
-			{
-				var output = compare(hostname);
-				
-				return ;
+	else{var output= compare(hostname);
+				return output ;
 			}
 }
 
-function compare(link)
-{	fetchData();
-	async function fetchData() 
-	{
-		const res=await fetch ("https://raw.githubusercontent.com/authifyWeb/authifyURL-Database/main/everything.json");
-			const response=await res.json();
-			var json = response.source;
-			for (i=0; i< json.length; i++)
-			{ var cl = response.source[i].urls;
-				for (j = 0; j<cl.length; j++)
-				{	
-					//console.log(link)
-					if(json[i].urls[j] == link )
-					{ 
-						var Data=`<div style="color:white";>  ` + link + `</br> <p> <span style="color:#A2FB15; font-size: 16px; ">Verified by authifyURL.</span> &nbsp;<span class="tooltip"> ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span>Organisation : </span>` +response.source[i].name+ `</p></div>` ;
-						
-						var Disclaimer=`Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white"; target ="_blank"> how we verify</a> what is valid and what is not. </br><div class="refresh_btn_box" style="margin-top:10px"><button type="button"; onClick="window.location.reload()">Search Another Link </button></div> `;
-						data.innerHTML= Data;
-						disclaimer.innerHTML=Disclaimer;
-					return;
-					}
-					else{
-						var Data = `<div style=" color:white"; >` + link + `<p><span style="color:red;font-size: 16px;"> We weren't able to verify this site. Please be cautioned continuing in this site.</span> &nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">Possibly scam. If unsure please take help from someone you know. </span> </p> </div>` ;
-						var Disclaimer=`Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white;" target ="_blank"> how we verify</a> what is valid and what is not. </br><div class="refresh_btn_box" style="margin-top:10px"><button type="button"; onClick="window.location.reload()">Search Another Link </button></div> `;
-						data.innerHTML= Data;
-						disclaimer.innerHTML=Disclaimer;
-						}
+function compare(link){
+var json = authData; 
 
-					
-				}
-			}
-		}
-			
-			
-			
-			
-			
+var new_link=link;
+var cl= json.urls;
+
+
+for(i=0;i<cl.length;i++){
+
+    if(json.urls[i]===link)
+      {//console.log("Yay")
+      var Data=`<div style="color:white;font-size:12px; background-color:#1f282d;">  ` + new_link + `</br> <p> <span style="color:#A2FB15; font-size: 14px; ">Verified by authifyURL.</span> &nbsp;<span class="tooltip" > ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span style="font-size:14px; color:white;">The page you submitted belongs to: </span><br> <span class="op_logo"><img src=${json.l}></span><span style="font-size:18px; color: #DFb014"> ` +json.name+ `</span></p></br></div>` ;
+                
+                var Disclaimer=`Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white"; target ="_blank"> how we verify</a> what is valid and what is not. </br>`;
+                data.innerHTML= Data;
+                disclaimer.innerHTML=Disclaimer;
+				no_promote.innerHTML="";
+              return Data;
+      }
+} 
+ 
+    console.log("Nop")
+    var Data = `<div style=" color:white; background-color:#1f282d; font-size:12px;" >` + new_link + `<p><br><span style="color:red;  font-size: 18px;"> The page you submitted doesn't belong to the organisation selected above</span>&nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">Possibly scam. Report the page directly to the owner. </span> </p> </div>` ;
+	/*if(json.off != ""){
+		console.log("Not Empty");
+	var Official = `<div style="font-size:10px;">To visit the official website of <span> `+json.id+` </span>use <a href="${json.off}">`+ json.off+` </a></div>`;
+	official.innerHTML=Official;}*/
+  var Disclaimer=`<br>Read <a href ="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white;" target ="_blank"> how we verify</a> what is valid and what is not. </br> </div>`;
+  data.innerHTML= Data;
+
+  disclaimer.innerHTML=Disclaimer;
+  no_promote.innerHTML="";
+  return Data;
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
 
+/* Loader or the svg spinner is modified from the works of Utkarsh Verma https://github.com/n3r4zzurr0/svg-spinners (MIT)  */
