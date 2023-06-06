2da4ba6bc10a485c3a0b9ff6c63adaed0dcd0bd2
webVerts <webverts@protonmail.com>
2023-06-06 20:53:17 +0530

    Sumbit button error resolved Submit button showed `Uncaught ReferenceError: searchNow` on submitting data. This updates resolves the issue and user can use submit button to URL submit data

--- resources/script.js
+++ resources/script.js
@@ -80,12 +80,16 @@ document.getElementById("compatible").innerHTML ="";
 document.getElementById('naam').innerHTML = `<p style="font-size:1em;"> Submit URL below to verify whether the site belongs to <span style="color:#DFB014 "> ${name} </span></> </p>`;
 
 var form=`<input type='url' id='link_id'  autocomplete="off" placeholder="Paste the URL" > `;
-var btn=`<input type="submit" id="search_btn" value="Search 🔍" onClick="searchNow()" > <p style="color:white; font-size:12px; padding-top:2px;">Please use a valid URL format (with https://). Or else no data will be displayed.</p><div class="refresh_btn_box" style="margin-top:5px; margin-bottom:5px;"><button type="button"; onClick="window.location.reload()">Search Another Link </button></div> `;
+var btn = `<input type="submit" id="search_btn" value="Search 🔍"> <p style="color:white; font-size:12px; padding-top:2px;">Please use a valid URL format (with https://). Otherwise, no data will be displayed.</p><div class="refresh_btn_box" style="margin-top:5px; margin-bottom:5px;"><button type="button" id="refresh_btn">Search Another Link</button></div> `;
+
 input_form.innerHTML=form;
+document.getElementById('input_btn').addEventListener('click', searchNow);
 input_btn.innerHTML=btn;
 no_promote.innerHTML="";
 
-
+document.getElementById('refresh_btn').addEventListener('click', function() {
+  window.location.reload();
+});
 
 
 }
@@ -127,7 +131,18 @@ function searchNow()
 	
 	document.getElementById("data").innerHTML ="";
 	document.getElementById("disclaimer").innerHTML ="";
+  document.getElementById("error_message").innerHTML = ""; 
+
+  try {
     var url = new URL(input);
+    // URL format is valid, continue with your code logic
+  } catch (error) {
+    // URL format is not valid, show an error message
+    var err_msg= `<p id="err_msg"style="margin-top:5px;">Submit a valid URL (eg https://url.authifyweb.com).</p>`;
+    error_message.innerHTML=err_msg;
+    
+    return; // stop further execution
+  }
 	
     var hostname = url.hostname;
     var protocol = url.protocol;
