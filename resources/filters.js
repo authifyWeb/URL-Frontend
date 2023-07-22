39c4d8fdab59dfc9b66058630b8c4badf4ad6360
webVerts <webverts@protonmail.com>
2023-07-22 19:00:36 +0530

    Shows warning for video URL and shorts video links.
    When video link or link for shorts video is submitted by the end user, the result will be a warning message, that only proifle links can be verified.

--- resources/filters.js
+++ resources/filters.js
@@ -47,6 +47,7 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search)
       {
         var channel=(pathname.split('/')[1]);
         if(channel=="channel" ||channel=="user" ) { link = hostname +'/' +pathname.split('/')[1]+ '/' + pathname.split('/')[2];}
+        else if(channel=="shorts"|| channel=="watch"){return `<p>Cannot identify individual videos. Please submit the profile URL to verify.</p>`;}
         else if(channel=="c") { 
         var id=pathname.split('/')[2].toLowerCase();
         link= hostname+'/'+id}
