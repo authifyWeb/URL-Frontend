b3c338808b784953fa82c5e28bd45a62fe565246
webVerts <webverts@protonmail.com>
2023-09-24 21:33:22 +0530

    Add keka.com filter
    fixes #99

--- resources/filters.js
+++ resources/filters.js
@@ -216,6 +216,11 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
     var output = compare(link,link);
     return output;
   }
+  else if(domain == "keka.com"){
+    link=hostname;
+    var output= compare(link,link);
+    return output;
+  }
   
 	else{
     link=domain;
