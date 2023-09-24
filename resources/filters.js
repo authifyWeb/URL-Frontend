faa5f2d189d9b82ed5955c9ab7474742dc6e1077
webVerts <webverts@protonmail.com>
2023-09-24 15:14:07 +0530

    Add support to linktr.ee sites
    Related #99

--- resources/filters.js
+++ resources/filters.js
@@ -125,7 +125,19 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
 				var output = compare(link,link); 
 			  return output;
 
-      }      
+      }
+  else if(hostname == "linktr.ee"){
+    var path1= pathname.split('/')[1].toLowerCase();
+    if(path1 !== "s" && path1 !=="marketplace" && path1 !== "blog" && path1 !=="help" && path1 !== "login" && path1 !=="register" & path1 !=="")
+        {
+          link=hostname+ pathname.toLowerCase();
+        }
+        else {
+          link = hostname;
+        }
+        var output = compare(link,link); 
+        return output;
+  }          
   else if(origin == "https://ko-fi.com" || origin =="https://www.buymeacoffee.com" || origin=="https://liberapay.com" || origin =="https://opencollective.com")
 				{
 					
