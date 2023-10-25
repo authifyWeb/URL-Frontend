ef8943a2c95c8aca62602d08ff9131d678552470
webVerts <webverts@protonmail.com>
2023-10-25 14:32:04 +0530

    Add graphy filter Fixes #126

--- resources/filters.js
+++ resources/filters.js
@@ -244,7 +244,11 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
     return output;
   }
 
-  
+  else if(domain=="graphy.com"){
+    link=hostname;
+    var output=compare(link,link);
+    return output;
+  } 
 	else{
     link=domain;
     
