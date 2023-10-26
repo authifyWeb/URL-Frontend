e42862387f4afc07d69b7a2cec01b2a0d284df1e
webVerts <webverts@protonmail.com>
2023-10-26 16:30:37 +0530

    Add public sourced Notice Fixes #121

--- resources/script.js
+++ resources/script.js
@@ -201,7 +201,7 @@ for (var i = 0; i < cl.length; i++) {
   if (json.urls[i] === link) {
     var Data = `<div style="color:white;font-size:12px; background-color:#1f282d;">` + display_link + `</br> <p> <span style="color:#A2FB15; font-size: 14px; ">Verified by authifyURL.</span> &nbsp;<span class="tooltip" > ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span style="font-size:14px; color:white;">The page you submitted belongs to: </span><br> <span class="op_logo"><img src="${json.logo}"></span><span style="font-size:18px; color: #DFb014"> ${json.name}</span></p></br></div>`;
                 
-    var Disclaimer = `Read <a href="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white"; target="_blank"> how we verify</a> what is valid and what is not. </br>`;
+    var Disclaimer = `<p style="color:yellow;">Unless explicitly mentioned with an "Official" tag, all data is obtained from the public domain. </p></br>`;
     
     data.innerHTML = Data;
     disclaimer.innerHTML = Disclaimer;
@@ -215,7 +215,7 @@ for (var i = 0; i < cl.length; i++) {
 if (!conditionMet) {
   var Data = `<div style="color:white; background-color:#1f282d; font-size:12px;" >` + display_link+`<p><br><span style="color:red;  font-size: 18px;"> The page you submitted doesn't belong to the organization selected above</span>&nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">Possibly a scam. Report the page directly to the owner. </span> </p> </div>`;
     
-  var Disclaimer = `<br>Read <a href="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white;" target="_blank"> how we verify</a> what is valid and what is not. </br>`;
+  var Disclaimer = `<br><p style="color:yellow;">Unless explicitly mentioned with an "Official" tag, all data is obtained from the public domain.</p></br>`;
     
   data.innerHTML = Data;
   disclaimer.innerHTML = Disclaimer;
