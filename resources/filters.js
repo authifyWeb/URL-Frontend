e4b8276f87422896ec08d0c383dd1c5a3cbb3155
webVerts <webverts@protonmail.com>
2023-11-04 19:19:33 +0530

    Add loco.gg filter

--- resources/filters.js
+++ resources/filters.js
@@ -164,6 +164,14 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
         var output = compare(link,link); 
         return output;
   }
+  else if(hostanem=="loco.gg"){ 
+    var path1=pathname.split('/')[1];
+    if(path1=="stream"|| path1=="clips"){return `<p style="color:yellow;">Cannot verify individual streams or clips. Please submit the profile URL to verify.</p>`;}
+    else if(path1=="streamers"){link=hostname+'/'+pathname.split('/')[2];}
+    else{link=domain;}
+    var output=compare(link,href);
+    return output; 
+  }
   else if(hostname=="www.t.me" || hostname == "www.telegram.me")
   { path1 = pathname.split('/')[1].toLowerCase();
     if(path1 =="s"){link= hostname + '/'+ pathname.split('/')[2].toLowerCase();}
