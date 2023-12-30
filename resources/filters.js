7fa2f177a0aa4c882785c5d6fafe3ca61e811191
webVerts <webverts@protonmail.com>
2023-12-31 00:03:25 +0530

    Add exlyapp

--- resources/filters.js
+++ resources/filters.js
@@ -305,7 +305,11 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
     else if(domain=="notion.site"){
       link=hostname;
       var output = compare(link,href); return output;
-    }        
+    }
+    else if(domain=="exlyapp.com"){
+      link=hostname; var output=compare(link,href); return output;
+    }
+
 
   else if(hostname == "addons.mozilla.org")
         /*Once the above condition is true, the function replaces the URL language to the the default en-US. This is used since mozilla supports multiple languages and the url structure is directly based on user language.  */
